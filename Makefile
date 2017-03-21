cea022182fd990b119d04b486c1ff212378797f1
Linus Torvalds <torvalds@linux-foundation.org>
2017-03-21 15:16:31 -0700

    Try to create test of truly random list walker
    .. with hugetlb and without.
    
    It may be broken.

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 test-tlb: test-tlb.c
-	gcc -Wall -O test-tlb.c -o test-tlb -lm
+	gcc -g -Wall -O test-tlb.c -o test-tlb -lm
 
 run: test-tlb
-	for i in 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 16M 32M 64M 128M ; do echo -n "$$i: "; ./test-tlb $$i 4k; done
+	for i in 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 16M 32M 64M 128M ; do echo -n "$$i: "; ./test-tlb -r $$i 64; done
+
+#
+# 15485863 is a random prime number that is used as a index into
+# the 128MB array. 15485863*4=61943452
+odd-case: test-tlb
+	./test-tlb 128M 61943452
--- test-tlb.c
+++ test-tlb.c
@@ -11,10 +11,14 @@
 #include <errno.h>
 #include <signal.h>
 #include <math.h>
+#include <time.h>
 
 #define PAGE_SIZE 4096
 
 #define FREQ 3.9
+
+static int test_hugepage = 0;
+static int random_list = 0;
 
 static void die(const char *fmt, ...)
 {
@@ -41,11 +45,12 @@ static void do_test(void *map, unsigned long size)
 
 	do {
 		count++;
-		offset += *(unsigned int *)(map + offset);
+		offset = *(unsigned int *)(map + offset);
 		offset &= size-1;
 	} while (!stop);
 	cycles = lrint(10*(double)FREQ*1000*1000*1000 / count);
-	printf("%lu.%06lu iterations (~%lu.%lu cycles) in one second, got to %lu\n",
+	printf("%.5fns %lu.%06lu iterations (~%lu.%lu cycles) in one second, got to %lu\n",
+		1000000000.0 / count,
 		count / 1000000,
 		count % 1000000,
 		cycles / 10,
@@ -81,9 +86,21 @@ static unsigned long get_num(const char *str)
 	return val;
 }
 
-static void *create_mmap(int fd, unsigned long size, unsigned long stride)
+static void *create_map(unsigned long size, unsigned long stride)
 {
+	int fd, i;
 	unsigned long off;
+
+	fd = open(".", O_RDWR | O_TMPFILE | O_EXCL, 0);
+	if (fd < 0)
+		die("tempfile creation failed");
+	for (i = 0; i < PAGE_SIZE / 4; i++) {
+		unsigned int data = stride;
+		if (write(fd, &data, 4) != 4)
+			die("stride write failed");
+	}
+
+
 	void *base = mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (base == MAP_FAILED)
 		die("base mmap failed");
@@ -95,24 +112,103 @@ static void *create_mmap(int fd, unsigned long size, unsigned long stride)
 	return base;
 }
 
+// Hugepage size
+#define HUGEPAGE (2*1024*1024)
+
+static void *create_random_map(unsigned long size, unsigned long stride)
+{
+	unsigned int *lastpos;
+	unsigned int flags = MAP_PRIVATE | MAP_ANONYMOUS;
+	unsigned long off, mapsize;
+	unsigned int *rnd;
+	void *map;
+	int n;
+
+	rnd = calloc(size / stride + 1, sizeof(unsigned int));
+	if (!rnd)
+		die("out of memory");
+	for (n = 0, off = 0; off < size; n++, off += stride)
+		rnd[n] = off;
+	for (n = 0, off = 0; off < size; n++, off += stride) {
+		unsigned int m = (unsigned long)random() % (size / stride);
+		unsigned int tmp = rnd[n];
+		rnd[n] = rnd[m];
+		rnd[m] = tmp;
+	}
+
+	mapsize = size;
+	if (test_hugepage)
+		mapsize += 2*HUGEPAGE;
+
+	map = mmap(NULL, mapsize, PROT_READ | PROT_WRITE, flags, -1, 0);
+	if (map == MAP_FAILED)
+		die("mmap failed");
+
+	if (test_hugepage) {
+		unsigned long mapstart = (unsigned long) map;
+		mapstart += HUGEPAGE-1;
+		mapstart &= ~(HUGEPAGE-1);
+		map = (void *)mapstart;
+
+		mapsize = size + HUGEPAGE-1;
+		mapsize &= ~(HUGEPAGE-1);
+
+		madvise(map, mapsize, MADV_HUGEPAGE);
+	}
+
+	/* Create a random mapping */
+	srandom(time(NULL));
+
+	/* Create a circular list of offsets */
+	lastpos = map;
+	for (n = 0, off = 0; off < size; n++, off += stride) {
+		lastpos = map + rnd[n];
+		*lastpos = rnd[n+1];
+	}
+	*lastpos = rnd[0];
+
+	free(rnd);
+
+	return map;
+}
+
 int main(int argc, char **argv)
 {
-	int fd, i;
 	unsigned long stride, size;
+	const char *arg;
+	void *map;
+
+	while ((arg = argv[1]) != NULL) {
+		if (*arg != '-')
+			break;
+		for (;;) {
+			switch (*++arg) {
+			case 0:
+				break;
+			case 'H':
+				test_hugepage = 1;
+			/* fall-through: hugepage implies random-list */
+			case 'r':
+				random_list = 1;
+				break;
+			default:
+				die("Unknown flag '%s'", arg);
+			}
+			break;
+		}
+		argv++;
+	}
 
 	size = get_num(argv[1]);
 	stride = get_num(argv[2]);
 	if (!stride || stride & 3 || size < stride)
-		die("bad arguments: test-tlb <size> <stride>");
-	fd = open(".", O_RDWR | O_TMPFILE | O_EXCL, 0);
-	if (fd < 0)
-		die("tempfile creation failed");
-	for (i = 0; i < PAGE_SIZE / 4; i++) {
-		unsigned int data = stride;
-		if (write(fd, &data, 4) != 4)
-			die("stride write failed");
-	}
+		die("bad arguments: test-tlb [-H] <size> <stride>");
 
-	do_test(create_mmap(fd, size, stride), size);
+	if (random_list)
+		map = create_random_map(size, stride);
+	else
+		map = create_map(size, stride);
+
+	do_test(map, size);
 	return 0;
 }
